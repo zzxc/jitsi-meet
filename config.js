dd7e362cf93d5beb3a958c5a7a5802dce12245a7
hristoterezov <hristo@jitsi.org>
2014-08-29 12:38:46 +0300

    config

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 var config = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'hristo.jitsi.net',
         //anonymousdomain: 'guest.example.com',
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
+        muc: 'conference.hristo.jitsi.net', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.hristo.jitsi.net', // FIXME: use XEP-0030
         //call_control: 'callcontrol.jitsi-meet.example.com'
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//hristo.jitsi.net/http-bind', // FIXME: use xep-0156 for that
     desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
     chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
     minChromeExtVersion: '0.1', // Required version of Chrome extension
@@ -20,6 +20,5 @@ var config = {
 //    useRtcpMux: true,
 //    useBundle: true,
     enableRecording: false,
-    enableWelcomePage: false,
-    isBrand: false
+    enableWelcomePage: true
 };
